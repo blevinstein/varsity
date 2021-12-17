8863fb4febe3eb8995ae3a2dd2ed2ca968e87fd5
Brian Levinstein <blevinstein@gmail.com>
2021-12-16 23:59:04 -0500

    Switch from web3 to ethers. Transactions sending but not working correctly.

--- uniswap_optimism.js
+++ uniswap_optimism.js
@@ -1,27 +1,19 @@
 
+const ethers = require('ethers');
 const fs = require('fs');
 const https = require('https');
-const Web3 = require('web3');
 const { AlphaRouter } = require('@uniswap/smart-order-router');
-const { Ether, Token } = require('@uniswap/sdk-core');
+const { CurrencyAmount, Ether, Percent, Token, TradeType } = require('@uniswap/sdk-core');
 
 const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
 
+const UNISWAP_V3_SWAP_ROUTER_ADDRESS = "0xE592427A0AEce92De3Edee1F18E0157C05861564";
+
 // Optimism
 const CHAIN_ID = 10;
-const provider = new Web3.providers.HttpProvider(
-    'https://mainnet.optimism.io',
-    {
-      reconnect: {
-        auto: true,
-        delay: 3000, //ms
-        maxAttempts: 5,
-        onTimeout: false
-      }
-    });
-
-const web3 = new Web3(provider);
+const provider = new ethers.providers.JsonRpcProvider(
+    'https://mainnet.optimism.io');
 
 const router = new AlphaRouter({
   chainId: CHAIN_ID,
@@ -61,28 +53,59 @@ async function main() {
   const argv = yargs(hideBin(process.argv)).array('input').argv;
 
   const dryRun = argv.noDryRun === undefined;
-  const amount = parseFloat(argv.amount);
+  const rawAmount = parseFloat(argv.amount);
   const fromToken = argv.from;
   const toToken = argv.to;
 
   console.log(`Reading key from ${argv.key}`);
   const {address, privateKey} = JSON.parse(fs.readFileSync(argv.key));
+  let wallet;
   if (dryRun) {
     console.log('DRY RUN MODE');
   } else {
-    web3.eth.accounts.wallet.add(privateKey);
-  }
-
-  const balance = await web3.eth.getBalance(address);
-  if (balance < amount * 1e18) {
-    throw Error(`Unsufficient funds: ${balance / 1e18} < ${amount}`);
+    wallet = new ethers.Wallet(privateKey, provider);
   }
 
   const tokenData = await getJson('https://static.optimism.io/optimism.tokenlist.json');
-  const fromTokenData = getCurrency(fromToken, tokenData);
-  const toTokenData = getCurrency(toToken, tokenData);
-  console.log(fromTokenData);
-  console.log(toTokenData);
+  const fromCurrency = getCurrency(fromToken, tokenData);
+  const toCurrency = getCurrency(toToken, tokenData);
+
+  /* TODO: check token balances
+  const balance = await provider.getBalance(address);
+  if (balance < amount * 1e18) {
+    throw Error(`Unsufficient funds: ${balance / 1e18} < ${amount}`);
+  }
+  */
+
+  const route = await router.route(
+    CurrencyAmount.fromRawAmount(fromCurrency, rawAmount * 1e18),
+    toCurrency,
+    TradeType.EXACT_INPUT,
+    {
+      recipient: address,
+      slippageTolerance: new Percent(5, 100),
+      deadline: 100
+    }
+  );
+  console.log(`Found route! Cost \$${route.estimatedGasUsedUSD.toFixed()}`);
+
+  const transaction = {
+    data: route.methodParameters.calldata,
+    to: UNISWAP_V3_SWAP_ROUTER_ADDRESS,
+    value: route.methodParameters.value,
+    from: address,
+    gasPrice: route.gasPriceWei,
+    gasLimit: route.estimatedGasUsed * 1.5,
+  };
+
+  console.log(route);
+  console.log(transaction);
+
+  if (!dryRun) {
+    console.log(`Performing transaction...`);
+    const result = await wallet.sendTransaction(transaction);
+    console.log(result);
+  }
 
   console.log('Done');
 }
