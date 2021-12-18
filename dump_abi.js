734d0ae7b1db899dc4e39321e014a2b365630cb0
Brian Levinstein <blevinstein@gmail.com>
2021-12-17 22:27:10 -0500

    Improve dump_abi

--- dump_abi.js
+++ dump_abi.js
@@ -6,14 +6,23 @@ const web3 = new Web3();
 const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
 
+function typeOf(input) {
+  if (input.type != 'tuple') {
+    return input.type;
+  } else {
+    return `[${input.components.map(c => typeOf(c)).join(',')}]`;
+    //return input.internalType;
+  }
+}
+
 async function main() {
   const argv = yargs(hideBin(process.argv)).array('input').argv;
 
   if (argv.interface) {
     const interface = JSON.parse(fs.readFileSync(argv.interface));
     for (let method of interface.abi.filter(i => i.type == 'function')) {
-      const string = `${method.name}(${(method.inputs || []).map(i => i.type).join(',')})`;
-      console.log(`${string} => ${web3.eth.abi.encodeFunctionSignature(string)}`);
+      const string = `${method.name}(${(method.inputs || []).map(typeOf).join(',')})`;
+      console.log(`${string} => ${web3.eth.abi.encodeFunctionSignature(method)}`);
     }
   }
 
