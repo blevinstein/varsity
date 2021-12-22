037c992153863adbbb90b351dc8aafbb3896c3f0
Brian Levinstein <blevinstein@gmail.com>
2021-12-22 14:33:07 -0500

    Experimenting on Uniswap without AlphaRouter

--- uniswap_mainnet.js
+++ uniswap_mainnet.js
@@ -3,7 +3,12 @@ const ethers = require('ethers');
 const fs = require('fs');
 const https = require('https');
 const { AlphaRouter } = require('@uniswap/smart-order-router');
+const { Pool, Route } = require('@uniswap/v3-sdk');
 const { CurrencyAmount, Ether, Percent, Token, TradeType } = require('@uniswap/sdk-core');
+
+const { abi: QuoterABI } = require("@uniswap/v3-periphery/artifacts/contracts/lens/Quoter.sol/Quoter.json");
+const { abi: FactoryABI } = require("@uniswap/v3-core/artifacts/contracts/UniswapV3Factory.sol/UniswapV3Factory.json");
+const { abi: PoolABI } = require("@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json");
 
 const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
@@ -11,7 +16,10 @@ const { hideBin } = require('yargs/helpers');
 const INFURA_ID = "07bc63faa17b4eae96c758bca58cfa85";
 
 // Mainnet
-const UNISWAP_V3_SWAP_ROUTER_ADDRESS = "0xE592427A0AEce92De3Edee1F18E0157C05861564";
+// const UNISWAP_V3_SWAP_ROUTER_ADDRESS = "0xE592427A0AEce92De3Edee1F18E0157C05861564";
+const UNISWAP_V3_FACTORY_ADDRESS = "0x1F98431c8aD98523631AE4a59f267346ea31F984";
+const UNISWAP_V3_SWAP_ROUTER_ADDRESS = "0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45";
+const UNISWAP_QUOTER_ADDRESS = "0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6";
 //const CHAIN_ID = 1;
 //const provider = new ethers.providers.JsonRpcProvider(
 //    `https://mainnet.infura.io/v3/${INFURA_ID}`);
@@ -20,6 +28,10 @@ const UNISWAP_V3_SWAP_ROUTER_ADDRESS = "0xE592427A0AEce92De3Edee1F18E0157C058615
 const CHAIN_ID = 4;
 const provider = new ethers.providers.JsonRpcProvider(
     `https://rinkeby.infura.io/v3/${INFURA_ID}`);
+const quoterContract = new ethers.Contract(UNISWAP_QUOTER_ADDRESS, QuoterABI, provider);
+const factoryContract = new ethers.Contract(UNISWAP_V3_FACTORY_ADDRESS, FactoryABI, provider);
+
+const FEE = 3000;
 
 const router = new AlphaRouter({
   chainId: CHAIN_ID,
@@ -55,6 +67,17 @@ function getCurrency(symbol, tokenData) {
   }
 }
 
+async function getPoolData(poolContract) {
+  const [token0, token1, fee, liquidity, slot] = await Promise.all([
+    poolContract.token0(),
+    poolContract.token1(),
+    poolContract.fee(),
+    poolContract.liquidity(),
+    poolContract.slot0()
+  ]);
+  return { token0, token1, fee, liquidity, sqrtPriceX96: slot[0], tick: slot[1] };
+}
+
 async function main() {
   const argv = yargs(hideBin(process.argv)).array('input').argv;
 
@@ -76,6 +99,39 @@ async function main() {
   const fromCurrency = getCurrency(fromToken, tokenData);
   const toCurrency = getCurrency(toToken, tokenData);
 
+  const poolAddress = await factoryContract.getPool(
+      fromCurrency.address,
+      toCurrency.address,
+      FEE);
+  const poolContract = new ethers.Contract(poolAddress, PoolABI, provider);
+  const poolData = await getPoolData(poolContract);
+  const poolToken0 = fromCurrency.address == poolData.token0 ? fromCurrency : toCurrency;
+  const poolToken1 = fromCurrency.address == poolData.token1 ? fromCurrency : toCurrency;
+  const pool = new Pool(
+      poolToken0,
+      poolToken1,
+      poolData.fee,
+      poolData.sqrtPriceX96.toString(),
+      poolData.liquidity.toString(),
+      poolData.tick);
+
+  //const route = new Route([pool], fromCurrency, toCurrency);
+  /*
+  const quotedAmountOut = await quoterContract.callStatic.quoteExactInputSingle(
+      fromCurrency.address,
+      toCurrency.address,
+      FEE,
+      BigInt(rawAmount * 1e18).toString(),
+      0);
+  const trade = await Trade.createUncheckedTrade({
+    route,
+    inputAmount: CurrencyAmount.fromRawAmount(fromCurrency, rawAmount * 1e18),
+    outputAmount: CurrencyAmount.fromRawAmount(toCurrency, quotedAmountOut.toString()),
+    tradeType: TradeType.EXACT_INPUT,
+  });
+  console.log(trade);
+  */
+
   const route = await router.route(
     CurrencyAmount.fromRawAmount(fromCurrency, rawAmount * 1e18),
     toCurrency,
@@ -83,9 +139,10 @@ async function main() {
     {
       recipient: address,
       slippageTolerance: new Percent(5, 100),
-      deadline: Math.floor(new Date().getTime() / 1000) + /* 6 hours */ 6 * 60 * 60,
+      deadline: Math.floor(new Date().getTime() / 1000) + 6 * 60 * 60,
     }
   );
+  console.log(route);
   console.log(`Found route! Cost \$${route.estimatedGasUsedUSD.toFixed()}`);
 
   const transaction = {
@@ -97,7 +154,6 @@ async function main() {
     gasLimit: route.estimatedGasUsed * 1.5,
   };
 
-  console.log(route);
   console.log(transaction);
 
   if (!dryRun) {
