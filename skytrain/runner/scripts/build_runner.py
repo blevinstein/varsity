915ef408832fbfe332ec19757196ad21da61a434
Brian Levinstein <blevinstein@gmail.com>
2021-12-31 20:24:26 -0500

    Move configs back out of runners/ dir

--- skytrain/runner/scripts/build_runner.py
+++ skytrain/runner/scripts/build_runner.py
@@ -5,6 +5,8 @@ from zipfile import ZipFile
 def main():
   with ZipFile('deploy.zip', 'w') as f:
     f.write('../scripts/common.js', 'common.js')
+    for environment in ['test']:
+      f.write('../configs/%s.json' % environment, 'configs/%s.json' % environment)
     for root,dirs,files in os.walk('.'):
       for file in files:
         if file == 'deploy.zip':
--- skytrain/scripts/reset_test_db.sh
+++ skytrain/scripts/reset_test_db.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-source runner/configs/test.sh || exit 1
+source configs/test.sh || exit 1
 
 psql -w -f sql/drop_db.sql || exit 1
 psql -w -f sql/create_db.sql || exit 1
