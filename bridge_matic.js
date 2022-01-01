3cf9eb452ff56452dd3f78241b8ef942f31200ec
Brian Levinstein <blevinstein@gmail.com>
2022-01-01 17:13:53 -0500

    Add support for testnet in bridge_matic

--- bridge_matic.js
+++ bridge_matic.js
@@ -5,16 +5,12 @@ const fs = require('fs');
 const { POSClient, use } = require('@maticnetwork/maticjs');
 const { Web3ClientPlugin } = require('@maticnetwork/maticjs-ethers');
 
+const INFURA_ID = "07bc63faa17b4eae96c758bca58cfa85";
 
 use(Web3ClientPlugin);
 
 const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
-
-const ethProvider = new ethers.providers.JsonRpcProvider(
-    `https://cloudflare-eth.com`);
-const maticProvider = new ethers.providers.JsonRpcProvider(
-    'https://polygon-rpc.com');
 
 function longToString(number) {
   return BigInt(number).toString();
@@ -25,9 +21,24 @@ async function main() {
 
   const dryRun = argv.noDryRun === undefined;
   const amount = parseFloat(argv.amount);
+  const mainnet = argv.mainnet !== undefined;
+
+  console.log(mainnet
+      ? 'Setting up mainnet...'
+      : 'Setting up testnet...');
+  const ethProvider = mainnet
+      ? new ethers.providers.JsonRpcProvider('https://cloudflare-eth.com')
+      : new ethers.providers.JsonRpcProvider(`https://goerli.infura.io/v3/${INFURA_ID}`);
+  const maticProvider = new ethers.providers.JsonRpcProvider(mainnet
+      ? 'https://polygon-rpc.com'
+      : 'https://rpc-mumbai.maticvigil.com/v1/5879797d6e4f3d3742e5ca26a1cfd3d38caa26e8');
 
   console.log(`Reading key from ${argv.key}`);
   const {address, privateKey} = JSON.parse(fs.readFileSync(argv.key));
+
+  const l1Balance = parseInt(await ethProvider.getBalance(address));
+  const l2Balance = parseInt(await maticProvider.getBalance(address));
+  console.log(`Balances: ${l1Balance / 1e18} (L1) ${l2Balance / 1e18} (L2)`);
 
   let posClient;
   if (dryRun) {
@@ -47,7 +58,6 @@ async function main() {
       },
     });
   }
-
 
   if (argv.token === undefined || argv.token == 'ETH') {
     console.log(`Bridge ${amount} ETH for address ${address}`);
