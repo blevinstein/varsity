34438858c8c9542d619b0841f34c594b38a63efc
Brian Levinstein <blevinstein@gmail.com>
2022-01-01 15:27:15 -0500

    Hide config from logs for security

--- skytrain/runner/index.js
+++ skytrain/runner/index.js
@@ -23,7 +23,7 @@ exports.run = async (input) => {
 
   const client = new Client(config);
   await client.connect();
-  console.log(`Connected! Region is ${region}\nConfig: ${JSON.stringify(config)}`);
+  console.log(`Connected! Region is ${region}`);
 
   const result = await client.query({
     text: 'SELECT a.Address,a.PrivateKey,o.Priority,o.Details,o.Status FROM Accounts a JOIN Operations o USING (Address) ' +
