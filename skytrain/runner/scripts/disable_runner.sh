4406cb38c24c75b46f60c8d3c4cca2a94d550fc6
Brian Levinstein <blevinstein@gmail.com>
2022-01-01 15:15:22 -0500

    Add scripts for enabling/disabling runner rules

---
+++ skytrain/runner/scripts/disable_runner.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ]; then
+REGIONS="us-west-1 us-east-2 ap-south-1 eu-central-1 eu-west-2 eu-west-3 me-south-1 sa-east-1"
+else
+REGIONS="$1"
+fi
+
+for region in $REGIONS; do
+
+echo "Disabling rule skytrain-runner-$region"
+aws events disable-rule \
+    --region "$region" \
+    --name skytrain-runner-$region
+
+done
