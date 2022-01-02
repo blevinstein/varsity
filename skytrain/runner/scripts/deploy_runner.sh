21b9616abf04c16920a8bc67de3d9cd2317acc70
Brian Levinstein <blevinstein@gmail.com>
2022-01-01 21:23:49 -0500

    Improve region support in deploy_runner script

--- skytrain/runner/scripts/deploy_runner.sh
+++ skytrain/runner/scripts/deploy_runner.sh
@@ -2,7 +2,15 @@
 
 set -e # Fail fast
 
-for region in us-west-1 us-east-2 ap-south-1 eu-central-1 eu-west-2 eu-west-3 me-south-1 sa-east-1; do
+if [ -z "$1" ]; then
+REGIONS="us-west-1 us-east-2 ap-south-1 eu-central-1 eu-west-2 eu-west-3 me-south-1 sa-east-1"
+else
+REGIONS="$1"
+fi
+
+for region in $REGIONS; do
+
 echo "deploy to skytrain-runner-$region"
-aws s3 cp deploy.zip s3://skytrain-runner-code-$region/deploy.zip
+aws s3 cp --region "$region" deploy.zip s3://skytrain-runner-code-$region/deploy.zip
+
 done
