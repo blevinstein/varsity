8a121c05e8ea3f5d4cd6c5a2c53890ca4ad69b73
Brian Levinstein <blevinstein@gmail.com>
2022-01-05 18:06:22 -0500

    Add multi-account support to bridge_matic script

--- bridge_matic.js
+++ bridge_matic.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
 
-const { bridge } = require('./skytrain/runner/chains/matic');
+const { bridge, checkBalance } = require('./skytrain/runner/chains/matic');
 const { check, longToString } = require('./skytrain/scripts/common');
 
 async function main() {
@@ -14,24 +14,38 @@ async function main() {
   const dryRun = argv.noDryRun === undefined;
   const amount = parseFloat(argv.amount);
   const mainnet = argv.mainnet !== undefined;
-  console.log(`Reading key from ${argv.key}`);
-  const {address, privateKey} = JSON.parse(fs.readFileSync(argv.key));
+  const accounts = argv.key
+    ? [JSON.parse(fs.readFileSync(argv.key))]
+    : JSON.parse(fs.readFileSync(argv.accounts));
 
-  const bridgeOptions = {
-    amount: amount * 1e18,
-    network: mainnet ? 'mainnet' : 'testnet',
-    address,
-    privateKey,
-    token: argv.token
-  };
+  for (let account of accounts) {
+    const {address, privateKey} = account;
+    console.log(`Address: ${address}`);
 
+    console.log('Checking balance');
+    const checkOptions = {
+      amount: amount * 1e18,
+      network: mainnet ? 'mainnet' : 'testnet',
+      address,
+      token: argv.token
+    };
+    await checkBalance(checkOptions);
 
-  let posClient;
-  if (dryRun) {
-    console.log('DRY RUN MODE');
-    console.log(bridgeOptions);
-  } else {
-    await bridge(bridgeOptions);
+    const bridgeOptions = {
+      amount: amount * 1e18,
+      network: mainnet ? 'mainnet' : 'testnet',
+      address,
+      privateKey,
+      token: argv.token
+    };
+
+    let posClient;
+    if (dryRun) {
+      console.log('DRY RUN MODE');
+      console.log(bridgeOptions);
+    } else {
+      await bridge(bridgeOptions);
+    }
   }
 
   console.log('Done');
